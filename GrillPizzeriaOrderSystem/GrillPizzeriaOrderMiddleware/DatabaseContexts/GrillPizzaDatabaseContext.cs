95cd987f58d306db37f60a6721d92ffac9e35e79
Ivan Marelja <imarelja@algebra.hr>
2025-08-07 17:50:11 +0200

    Mapping profile and Database are finished finished

--- GrillPizzeriaOrderSystem/GrillPizzeriaOrderMiddleware/DatabaseContexts/GrillPizzaDatabaseContext.cs
+++ GrillPizzeriaOrderSystem/GrillPizzeriaOrderMiddleware/DatabaseContexts/GrillPizzaDatabaseContext.cs
@@ -1,11 +1,71 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace GrillPizzeriaOrderMiddleware.DatabaseContexts
 {
     public class GrillPizzaDatabaseContext : DbContext
     {
-        public GrillPizzaDatabaseContext(DbContextOptions<GrillPizzaDatabaseContext> context) : base(context) {}
+        public GrillPizzaDatabaseContext(DbContextOptions<GrillPizzaDatabaseContext> context) 
+            : base(context) {}
+
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<OrderFood> OrderFoods { get; set; }
+        public DbSet<Allergen> Allergens { get; set; }
+        public DbSet<FoodCategory> FoodCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
 
+            // Tables (M:N)
+
+            modelBuilder.Entity<OrderFood>()
+                .HasKey(of => new { of.OrderId, of.FoodId });
+
+            modelBuilder.Entity<OrderFood>()
+                .HasOne(of => of.Order)
+                .WithMany(o => o.OrderFoods)
+                .HasForeignKey(of => of.OrderId);
+
+            modelBuilder.Entity<OrderFood>()
+                .HasOne(of => of.Food)
+                .WithMany(f => f.OrderFoods)
+                .HasForeignKey(of => of.FoodId);
+
+            modelBuilder.Entity<FoodAllergen>()
+                .HasKey(fa => new { fa.FoodId, fa.AllergenId });
+
+            modelBuilder.Entity<FoodAllergen>()
+                .HasOne(fa => fa.Food)
+                .WithMany(f => f.FoodAllergens)
+                .HasForeignKey(fa => fa.FoodId);
+
+            modelBuilder.Entity<FoodAllergen>()
+                .HasOne(fa => fa.Allergen)
+                .WithMany(a => a.FoodAllergens)
+                .HasForeignKey(fa => fa.AllergenId);
+
+
+            // Tables (1:N)
+
+            modelBuilder.Entity<Food>()
+                .HasOne(f => f.FoodCategory)
+                .WithMany(fc => fc.Foods)
+                .HasForeignKey(f => f.FoodCategoryId);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+        }
     }
 }
--- GrillPizzeriaOrderSystem/GrillPizzeriaOrderMiddleware/MappingProfile.cs
+++ GrillPizzeriaOrderSystem/GrillPizzeriaOrderMiddleware/MappingProfile.cs
@@ -1,11 +1,64 @@
 ﻿using AutoMapper;
+using DTO.Allergen;
+using DTO.Food;
+using DTO.FoodCategory;
+using DTO.Order;
+using Microsoft.Extensions.Options;
+using Models;
 
 namespace GrillPizzeriaOrderMiddleware
 {
     public class MappingProfile : Profile
     {
-        public MappingProfile() { 
-        
+        public MappingProfile() {
+
+            // Food
+            CreateMap<Food, FoodReadDto>()
+                .ForMember(destination => destination.FoodCategoryName,
+                            options => options.MapFrom(
+                                source => source.FoodCategory.Name
+                                )
+                            )
+                .ForMember(destination => destination.AllergenNames, 
+                            option => option.MapFrom(
+                                source => source.FoodAllergens.Select(fa => fa.Allergen.Name).ToList()
+                                )
+                            );
+
+            CreateMap<FoodCreateDto, Food>()
+                .ForMember(destination => destination.FoodAllergens, options => options.Ignore());
+
+            CreateMap<FoodUpdateDto, Food>()
+                .ForMember(destination => destination.FoodAllergens, options => options.Ignore());
+
+            // FoodCategory
+            CreateMap<FoodCategory, FoodCategoryReadDto>();
+            CreateMap<FoodCategoryCreateDto, FoodCategory>();
+
+            // Allergen
+            CreateMap<Allergen, AllergenReadDto>();
+            CreateMap<AllergenCreateDto, Allergen>();
+
+            // Order
+            CreateMap<Order, OrderReadDto>()
+                .ForMember(destination => destination.Username,
+                            options => options.MapFrom(
+                                source => source.User.Username
+                                )
+                            )
+                .ForMember(destination => destination.FoodNames,
+                            options => options.MapFrom(
+                                source => source.OrderFoods.Select(af => af.Food.Name).ToList()
+                                )
+                            );
+
+            CreateMap<OrderCreateDto, Order>()
+                .ForMember(destination => destination.OrderFoods, options => options.Ignore())
+                .ForMember(destination => destination.OrderDate, 
+                            options => options.MapFrom(
+                                _ => DateTime.UtcNow
+                                )
+                            );
         }
     }
 }
