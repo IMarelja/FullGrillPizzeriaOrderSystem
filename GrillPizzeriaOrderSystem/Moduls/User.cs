4e390bac8c278e962bd6b23ef5c55178275beb9e
Ivan Marelja <imarelja@algebra.hr>
2025-08-17 22:37:00 +0200

    CreationDate typo

--- GrillPizzeriaOrderSystem/Moduls/User.cs
+++ GrillPizzeriaOrderSystem/Moduls/User.cs
@@ -38,7 +38,7 @@ namespace Models
         [StringLength(ValidationConstants.PhoneMaxLength)]
         [Phone]
         public string Phone { get; set; } = string.Empty;
-        public DateTime DateCreation { get; set; }
+        public DateTime CreationDate { get; set; }
 
         public int RoleId { get; set; }
         public Role Role { get; set; } = null!;
