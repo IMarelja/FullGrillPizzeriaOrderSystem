ddc78eb9e6c027a9b290daf39ce077e4d048ed43
Ivan Marelja <imarelja@algebra.hr>
2025-08-18 19:11:51 +0200

    User information reading and updating

---
+++ GrillPizzeriaOrderSystem/DTO/User/UserReadDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.User
+{
+    public class UserReadDto
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+        public DateTime CreationDate { get; set; }
+
+        public string RoleName { get; set; } = string.Empty;
+    }
+}
---
+++ GrillPizzeriaOrderSystem/DTO/User/UserUpdateDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelConstants;
+
+namespace DTO.User
+{
+    public class UserUpdateDto
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(ValidationConstants.EmailMaxLength)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(ValidationConstants.NameMaxLength)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(ValidationConstants.NameMaxLength)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(ValidationConstants.PhoneMaxLength)]
+        [Phone]
+        public string Phone { get; set; } = string.Empty;
+    }
+
+}
