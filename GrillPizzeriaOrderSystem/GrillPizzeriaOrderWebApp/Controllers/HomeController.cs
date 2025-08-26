2491d2acaff735854071defe051d8c3313423b72
Ivan Marelja <imarelja@algebra.hr>
2025-08-26 18:22:44 +0200

    Menu displayed configuration base

--- GrillPizzeriaOrderSystem/GrillPizzeriaOrderWebApp/Controllers/HomeController.cs
+++ GrillPizzeriaOrderSystem/GrillPizzeriaOrderWebApp/Controllers/HomeController.cs
@@ -18,15 +18,17 @@ namespace GrillPizzeriaOrderWebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+
     }
 }
---
+++ GrillPizzeriaOrderSystem/GrillPizzeriaOrderWebApp/Controllers/MenuController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ViewModels;
+
+namespace GrillPizzeriaOrderWebApp.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IHttpClientFactory _http;
+        public MenuController(IHttpClientFactory http) => _http = http;
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var client = _http.CreateClient("DataAPI");
+
+            List<FoodViewModel> foodList;
+            List<CategoryFoodViewModel> categoryFoodList;
+
+            try
+            {
+                categoryFoodList = new List<CategoryFoodViewModel>();
+            }
+            catch
+            {
+                categoryFoodList = new List<CategoryFoodViewModel>();
+            }
+
+            ViewBag.CategoryFoodList = categoryFoodList;
+
+            try
+            {
+                foodList = new List<FoodViewModel>();
+            }
+            catch
+            {
+                foodList = new List<FoodViewModel>();
+            }
+
+            ViewBag.FoodList = foodList;
+
+            return View();
+        }
+    }
+}
