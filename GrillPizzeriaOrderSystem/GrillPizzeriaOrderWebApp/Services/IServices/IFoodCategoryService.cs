ec1fb2909efecbe3ee2f14e913d7db2fbd5ee2ac
Ivan Marelja <imarelja@algebra.hr>
2025-09-04 17:45:37 +0200

    Minor function name change

--- GrillPizzeriaOrderSystem/GrillPizzeriaOrderWebApp/Services/IServices/IFoodCategoryService.cs
+++ GrillPizzeriaOrderSystem/GrillPizzeriaOrderWebApp/Services/IServices/IFoodCategoryService.cs
@@ -7,8 +7,8 @@ namespace GrillPizzeriaOrderWebApp.Services.IServices
     {
         Task<IEnumerable<CategoryFoodViewModel>> GetAll();
         Task<CategoryFoodViewModel?> GetById(int id);
-        Task<ApiOperationResult> UpdateAsync(CategoryFoodEditViewModel categoryFood);
         Task<ApiOperationResult<int>> CreateAsync(CategoryFoodCreateViewModel categoryFood);
+        Task<ApiOperationResult> UpdateAsync(CategoryFoodEditViewModel categoryFood);
         Task<ApiOperationResult> DeleteAsync(CategoryFoodDeleteViewModel categoryFood);
 
     }
