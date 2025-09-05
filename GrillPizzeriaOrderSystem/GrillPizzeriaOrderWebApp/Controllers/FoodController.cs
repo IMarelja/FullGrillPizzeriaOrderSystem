9c65b0fc8253c4174213cd1c565fb89ed9173b41
Ivan Marelja <imarelja@algebra.hr>
2025-09-05 22:18:55 +0200

    Food managment and licence

--- GrillPizzeriaOrderSystem/GrillPizzeriaOrderWebApp/Controllers/FoodController.cs
+++ GrillPizzeriaOrderSystem/GrillPizzeriaOrderWebApp/Controllers/FoodController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using ViewModels;
 
 namespace GrillPizzeriaOrderWebApp.Controllers
@@ -20,22 +21,90 @@ namespace GrillPizzeriaOrderWebApp.Controllers
             _foodService = foodService;
         }
 
+        [HttpPost]
+        
         public IActionResult Index()
-            => View("CreateFood.cshtml");
+            => View("~/Views/Food/CreateFood.cshtml");
 
+        [Authorize(Policy = "AdminOnly")]
+        public IActionResult CreateFood()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "AdminOnly")]
+        public IActionResult EditFood()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "AdminOnly")]
         public async Task<IActionResult> Create(FoodCreateViewModel food)
         {
             if (!ModelState.IsValid)
             {
-                var message = ApiOperationResult.Fail("Creation validation error.");
-                return View("~/Views/Food/CreateFood.cshtml", message);
+                var errors = ModelState
+                        .Where(ms => ms.Value?.Errors.Count > 0)
+                        .Select(ms => new
+                        {
+                            Field = ms.Key,
+                            Errors = ms.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
+                        });
+
+                return BadRequest(new
+                {
+                    success = false,
+                    message = "Invalid Food request",
+                    errors
+                });
             }
 
             var responce = await _foodService.CreateAsync(food);
 
-            return View("~/Views/Food/CreateFood.cshtml", new ApiOperationResult<FoodCreateViewModel> { Succeeded = true });
+            return Ok(new
+            {
+                success = responce.Succeeded,
+                data = responce,
+                message = responce.Message + " " + string.Join(", ", responce.Errors),
+            });
         }
 
+        [HttpPost]
+        [Authorize(Policy = "AdminOnly")]
+        public async Task<IActionResult> Edit(FoodEditViewModel food)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState
+                        .Where(ms => ms.Value?.Errors.Count > 0)
+                        .Select(ms => new
+                        {
+                            Field = ms.Key,
+                            Errors = ms.Value!.Errors.Select(e => e.ErrorMessage).ToArray()
+                        });
+
+                return BadRequest(new
+                {
+                    success = false,
+                    message = "Invalid Food request",
+                    errors
+                });
+            }
+
+            var responce = await _foodService.UpdateAsync(food);
+
+            return Ok(new
+            {
+                success = responce.Succeeded,
+                data = responce,
+                message = responce.Message + " " + string.Join(", ", responce.Errors),
+            });
+        }
+
+
+        [HttpPost]
+        [Authorize(Policy = "AdminOnly")]
         public async Task<IActionResult > Delete(int foodId)
         {
 
@@ -48,6 +117,18 @@ namespace GrillPizzeriaOrderWebApp.Controllers
                 return RedirectToAction("Index");
 
             return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var food = await _foodService.GetById(id);
+
+            if(food == null)
+                return NotFound( new { success = false , message = "Unsuccessful fetching of food item either it doesn't exist or server failers"});
+
+            return Ok( new { success = true , data = food });
         }
 
         [HttpGet]
